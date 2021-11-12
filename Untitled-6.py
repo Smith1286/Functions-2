27b1d27a24ee762bd8d377f5ec35a45d87a58d1f
Smith1286 <89523712+Smith1286@users.noreply.github.com>
2021-11-12 08:06:00 -0500

    Update Untitled-6.py

--- Untitled-6.py
+++ Untitled-6.py
@@ -1,32 +1,8 @@
-# Uncomment this when you reach the "Use the Force" section
-# train_mass = 22680
-# train_acceleration = 10
-# train_distance = 100
-# bomb_mass = 1
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station.")
+  print("Take the Northbound N, Q, R, or W train 1 stop.")
+  print("Get off the Times Square 42nd Street stop.")
+  print("Take lots of pictures!")
 
-
-# Write your code below: 
-def f_to_c(f_temp):
-    return (f_temp - 32) * 5/9
-
-f100_in_celsius = f_to_c(100)
-
-def c_to_f(c_temp):
-    return (c_temp * 9/5) + 32
-
-  c0_in_fahrenheit = c_to_f():
-print(c0_in_fahrenheit)
-
-def get_forces(mass, acceleration):
-    return mass * accleration
-
-train_force = get_force(train_mass,
-train_accelertion)
-
-print("The GE train supplies + str (train_force) + Newtons of force")
-
-def get_energy(mass, c+3*10**8):
-  return mass * c**2
-
-bomb_energy = get_energy(bomb_mass)  
-print("A 1kg bomb supplies") + str(bomb_energy) + " Joules "
+# Call your function here
+directions_to_timesSq()
